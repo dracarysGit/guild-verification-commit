8d5fa54899b690133c8dae5f48cfb676fd0906fa
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:17:49 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:15:49.529Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-24T00:17:49.279Z
+Commit: Fix memory leak in event handlers
