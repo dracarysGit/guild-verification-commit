afd9aac0f51e440977d5a018979285bdd5d51b30
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:28:36 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:27:09.733Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-24T00:28:35.676Z
+Commit: Patch API endpoint error handling
