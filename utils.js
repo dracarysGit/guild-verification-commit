bd00e16e05e35e9137af4799620f36441d7738e7
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:24:42 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:23:31.486Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-24T00:24:42.220Z
+Commit: Fix broken links in documentation
