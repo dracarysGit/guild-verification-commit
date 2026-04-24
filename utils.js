40782eae5efc493a7ba91457b20a273709b4901a
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:27:10 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:25:53.994Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-24T00:27:09.733Z
+Commit: Fix broken links in documentation
