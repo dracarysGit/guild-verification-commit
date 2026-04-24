09cb3395b8f69a443b20fb86e8ddfd7a0413a1f4
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:17:37 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:15:43.255Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-24T00:17:37.243Z
+Commit: Resolve null pointer exception in user service
