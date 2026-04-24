b12481dcd74f5223be9d01e0fd491f875cab4c0b
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:18:20 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:15:13.163Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-24T00:18:20.108Z
+Commit: Fix memory leak in event handlers
