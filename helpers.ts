45a5ea62f2c9e884e17a2d7baccd273487a2fb3d
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:28:37 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:27:13.769Z
-Commit: Add multi-language support
+Timestamp: 2026-04-24T00:28:36.686Z
+Commit: Resolve timezone issues in date handling
