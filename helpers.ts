c3d8149880b3cc7a50f17dfc54a8810409241f96
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:26:05 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:24:44.241Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-24T00:26:05.089Z
+Commit: Patch API endpoint error handling
