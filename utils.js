83b3af66004f459880603dbe8c5f0c00b45394dc
dracarysGit <dracarysnft@outlook.com>
2026-04-24 03:19:00 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:17:49.279Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-24T00:19:00.487Z
+Commit: Resolve null pointer exception in user service
